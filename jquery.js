2c1b25e60c05f496d9c2478b40bab199752d2f37
gazz31 <wojtekku@outlook.com>
2018-08-16 18:27:10 +0200

    thinking_about_everything
    consternation

--- jquery.js
+++ jquery.js
@@ -12,114 +12,97 @@ $(document).ready(function(){
 
 
 });
-$(document).ready(function(){
-    $("#text").load(function(){
-    
-             
-            $(document).ready(function()
-            {
-                step_0(); 
-            })
 
-      
 
-    });
 
-});
-function step_1() 
+
+
+function step_0() 
 {
-    var text_1 = "gora";
-    text_1.css({
-        "font-size": "26px",
-        "color": "red",
-        "font-Style": "italic",
-        "font-weight": "bolder"
-    });
-}
-function step_2() 
-{
-    var text_2 = "lewa";
-    text_2.css({
-        "font-size": "26px",
-        "color": "green",
-        "font-Style": "italic",
-        "font-weight": "bolder"
-    });
-}
-function step_3() 
-{
-    var text_3 = "dol";
-    text_3.css({
-        "font-size": "26px",
-        "color": "yellow",
-        "font-Style": "italic",
-        "font-weight": "bolder"
-    });
-
-
-}
-
-$("p#44.test").css("background-color","red");
-
-    function one()
-    {
-        
-        
-        
-        $("#moon_show").css({"visibility":"invisible"}),
-		$("#moon_show").delay(500),
-		$("#moon_show").animate({"left":"=0%"}),
-        $("#moon_show").animate({"left":"+=77.5%"}),
-
-        $("#moon_show").delay(500),
-		$("#moon_show").animate({"bottom":"=0%"}),
-		$("#moon_show").animate({"bottom":"+=72.2%"}),
- 
   
-        $("#moon_show").delay(500),
-		$("#moon_show").animate({"left":"=0%"}),
-		$("#moon_show").animate({"left":"-=-10.4%"}),
+		$("#moon_show").css({"visibility":"invisible"});
+		
+		$("#moon_show").delay(500).animate({
+
+		
+        "left":"+=77.5%"
         
-      
-        $("#moon_show").delay(500),
-		$("#moon_show").animate({"bottom":"=0%"}),
-        $("#moon_show").animate({"bottom":"+=28.8%"})
+    
+    });
 
-        $("#moon_show").delay(500),
-		$("#moon_show").animate({"left":"+=0"}),
-        $("#moon_show").animate({"left":"+=65%"}),
+        $("#moon_show").delay(800).animate({
+
+    
+            "bottom":"+=72%"
+    
+
+    });
+         $("#moon_show").delay(800).animate({
+
+    
+            "left":"-=-10.4%"
+
+    }); 
+    $("#moon_show").delay(800).animate({
+
+    
+        "bottom":"+=28.8%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "left":"+=65%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "bottom":"+=57.6%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "left":"+=23%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "bottom":"-=-43%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "left":"-=-65%"
+
+    });
+    $("#moon_show").delay(800).animate({
+
+    
+        "left":"+=+35%",
+       
+    });
+   
         
-        $("#moon_show").delay(500),
-        $("#moon_show").animate({"bottom":"+=0"}),
-        $("#moon_show").animate({"bottom":"+=57.6%"}),
-
         
-        $("#moon_show").delay(500),
-        $("#moon_show").animate({"left":"=0%"}),
-        $("#moon_show").animate({"left":"+=23%"}),
+        $("#moon_show").css({'background':'red'})
+           
         
-        $("#moon_show").delay(500),
-        $("#moon_show").animate({"bottom":"=0%"}),
-        $("#moon_show").animate({"bottom":"-=-43%"}),
+    
 
-        $("#moon_show").delay(500),
-        $("#moon_show").animate({"left":"=0%"}),
-        $("#moon_show").animate({"left":"-=-65%"}),
-
-        $("#moon_show").animate({"bottom":"+=0"}),
-        $("#moon_show").delay(1000),
-        $("#moon_show").animate({"bottom":"+=170px"}),
-        $("#moon_show").animate({"left":"+=0"}),
-        $("#moon_show").delay(1000),
-        $("#moon_show").animate({"left":"-=290px"}),
-        $("#moon_show").animate({"bottom":"+=0"}),
-        $("#moon_show").animate({"left":"-=80px"})
+}
 
 
-    }
+
+
+  
 
     function link()
     {
+       
         $(location).attr('href', 'one.html');
     }
 
@@ -151,11 +134,10 @@ if ($(document).ready())
     
         
             $("#go").click(function(){
-                setTimeout(one,1000);
+                step_0(); 
+                setTimeout(link,15000);
+              
                 
-                setTimeout(link,23000);
-            
-            
                 
             
             
